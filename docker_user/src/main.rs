a3bb22ecda3d8f9d7d4ba88ab0aac7ed88e5250d
Marius Kluften <mkluften93@gmail.com>
2023-09-29 13:57:59 +0200

    create a cargo project for a rust program that will be compiled in a docker file as opposed to wasm

---
+++ docker_user/src/main.rs
@@ -0,0 +1,20 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let num: usize = buffer.trim().parse().unwrap();
+    let fib_seq = fibonacci(num);
+    for num in fib_seq {
+        println!("{}", num);
+    }
+}
+
+fn fibonacci(n: usize) -> Vec<u64> {
+    let mut seq = vec![0, 1];
+    for i in 2..n {
+        let next = seq[i - 1] + seq[i - 2];
+        seq.push(next);
+    }
+    seq
+}
