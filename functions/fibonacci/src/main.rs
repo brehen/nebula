df5e9fd3bb2acfa8a60cdeb9a1709397d47b22db
Marius Kluften <mkluften93@gmail.com>
2023-10-01 11:01:36 +0200

    create folder for storing standalone functions that can be compiled to wasm

---
+++ functions/fibonacci/src/main.rs
@@ -0,0 +1,41 @@
+use std::io::{stdin, BufRead};
+
+fn main() {
+    let stdin = stdin();
+    let mut input = String::new();
+
+    stdin
+        .lock()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let parsed_input: Result<i32, _> = input.trim().parse();
+
+    let size = if let Ok(size) = parsed_input {
+        size
+    } else {
+        println!("Expected a number, got something else! Defaulting to 1");
+        1
+    };
+
+    let sequence = fibonacci(size);
+    println!("{:?}", sequence);
+}
+
+fn fibonacci(size: i32) -> Vec<u64> {
+    let mut sequence = Vec::<u64>::new();
+
+    for i in 0..size {
+        let j = i as usize;
+        if i == 0 || i == 1 {
+            sequence.push(i as u64);
+        } else {
+            let next_value = sequence[j - 1] + sequence[j - 2];
+            sequence.push(next_value);
+        }
+    }
+
+    // println!("Help me");
+
+    sequence
+}
