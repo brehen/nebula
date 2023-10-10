60810aed1c27924582f9efb45aefeb9c8b42bddb
Marius Kluften <marius@kluften.dev>
2023-10-10 22:54:01 +0200

    add function for streamlining running X module

--- web_server/src/utilities/run_wasm_module.rs
+++ web_server/src/utilities/run_wasm_module.rs
@@ -1,26 +1,53 @@
-use std::env;
-
 use axum::{extract::Path, response::Html};
-use nebula_lib::{
-    models::FunctionResult,
-    wasm_runner::{self},
-};
+use nebula_lib::wasm_runner::{self};
+use serde::Deserialize;
 
-pub fn run_wasm_module(size: &str) -> FunctionResult {
-    let cwd = env::current_dir().expect("cwd to exist");
-    let cwd = cwd.to_str().expect("To parse");
-    // let cwd = "/Users/mariuskluften";
-    let file_path = format!("{}/projects/wasm_modules/fib.wasm", cwd);
-    println!("{:?}", file_path);
-    wasm_runner::run_wasi_module(file_path.as_str(), size).expect("There to be a proper return")
-}
+use super::get_file_path::get_file_path;
 
 pub async fn run_fib_module(Path(size): Path<String>) -> Html<String> {
-    let sequence = run_wasm_module(size.as_str());
+    let file_path = get_file_path("fibonacci");
+    let add_path = get_file_path("add");
+
+    let sequence =
+        wasm_runner::run_wasi_module(&file_path, "5").expect("There to be a proper return");
+    let sequence2 =
+        wasm_runner::run_wasi_module(&file_path, "8").expect("There to be a proper return");
+
+    dbg!(format!("{},{}", sequence.result, sequence2.result).as_str());
+    let number = wasm_runner::run_wasi_module(
+        &add_path,
+        format!("{},{}", sequence.result, sequence2.result).as_str(),
+    )
+    .expect("There to be a proper return");
+
+    println!("number was: {}", number.result);
+
     let mut html = format!("<h1>Fibonacci</h1>");
     if let Some(metrics) = sequence.metrics {
         html = format!("<h1>Fib is nice! Here are the first {} numbers of the sequence:<h1><p>Total time: {}ms</p><p>Startup time: {}ms</p><p>{}</p>",
         size, metrics.total_runtime, metrics.startup_time, sequence.result);
     };
+
     Html(html)
+}
+
+#[derive(Deserialize)]
+pub struct Params {
+    module: String,
+    input: String,
+}
+
+pub async fn run_wasm_module(Path(Params { module, input }): Path<Params>) -> Html<String> {
+    let input = input.as_str();
+    let file_path = get_file_path(module.as_str());
+    match wasm_runner::run_wasi_module(&file_path, input) {
+        Ok(result) => Html(format!(
+            "<div><h1>{}, input: {}</h1><p>Result was: {}</p></div>",
+            module, input, result.result
+        )),
+        Err(err) => Html(format!(
+            "<p>Whoops! Error!</p> <p style=\"color:red;\">{}</p>",
+            err.to_string()
+        )),
+    }
 }
