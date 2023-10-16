8292748b7668c0d77b87a73d441c40ffd9043962
Marius Kluften <marius@kluften.dev>
2023-10-16 16:47:39 +0200

    rewrite shared lib to handle most functionality and parsing

--- examples/wasm/shared/src/lib.rs
+++ examples/wasm/shared/src/lib.rs
@@ -1,9 +1,28 @@
 use anyhow::{Context, Error, Result};
+
 use std::{
     any::type_name,
+    fmt::Display,
     io::{stdin, BufRead},
     str::FromStr,
 };
+
+mod docker {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    pub fn get_epoch_timestamp() -> Option<String> {
+        let now = SystemTime::now();
+        let dur_since_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
+        let msecs_since_epoch = dur_since_epoch.as_millis();
+        Some(format!("{}", msecs_since_epoch))
+    }
+}
+
+mod wasm {
+    pub fn get_epoch_timestamp() -> Option<String> {
+        None
+    }
+}
 
 /// Utility function for getting stdin and parse it to expected type, up to caller to decide
 /// Usage:
@@ -11,7 +30,7 @@ use std::{
 /// let input: i32 = get_stdin().expect("Failed to parse stdin, check value");
 /// ```
 
-pub fn get_stdin<T: FromStr>() -> Result<T> {
+fn get_stdin<T: FromStr>() -> Result<T> {
     let mut input = String::new();
 
     stdin()
@@ -23,4 +42,32 @@ pub fn get_stdin<T: FromStr>() -> Result<T> {
         .trim()
         .parse::<T>()
         .map_err(|_| Error::msg(format!("Failed to parse input as {}", type_name::<T>())))
+}
+
+fn print_stdout<T: Display>(output: T, timestamp: Option<String>) {
+    match timestamp {
+        Some(timestamp) => println!("{}|{}", output, timestamp),
+        None => println!("{}", output),
+    }
+}
+
+pub enum FunctionType {
+    Docker,
+    Wasm,
+}
+
+pub fn run_function<T, F, R>(func: F, func_type: FunctionType)
+where
+    T: FromStr,
+    F: Fn(T) -> R,
+    R: std::fmt::Display,
+{
+    let timestamp = match func_type {
+        FunctionType::Docker => docker::get_epoch_timestamp(),
+        FunctionType::Wasm => wasm::get_epoch_timestamp(),
+    };
+
+    let input: T = get_stdin().expect("To parse correctly");
+    let result = func(input);
+    print_stdout(result, timestamp)
 }
