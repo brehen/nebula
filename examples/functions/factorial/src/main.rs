df894ca9501014b1c8aaf230c793ca0d5a735f8b
Marius Kluften <marius@kluften.dev>
2024-01-15 20:51:15 +0100

    implement factorial example code

--- examples/functions/factorial/src/main.rs
+++ examples/functions/factorial/src/main.rs
@@ -1,3 +1,16 @@
+use shared::{run_function, FunctionType};
+
 fn main() {
-    println!("Hello, world!");
+    let func_type = if cfg!(feature = "docker") {
+        FunctionType::Docker
+    } else {
+        FunctionType::Wasm
+    };
+
+    run_function(factorial, func_type);
+}
+
+fn factorial(num: u128) -> String {
+    let fact: u128 = (1..=num).product();
+    format!("{:?}", fact)
 }
