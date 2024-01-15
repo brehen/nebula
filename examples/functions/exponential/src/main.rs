ba0d117586bca1c55cc45b7e38172f5c4bfd6457
Marius Kluften <marius@kluften.dev>
2024-01-15 20:51:08 +0100

    implement exponential example

--- examples/functions/exponential/src/main.rs
+++ examples/functions/exponential/src/main.rs
@@ -1,3 +1,16 @@
+use shared::{run_function, FunctionType};
+
 fn main() {
-    println!("Hello, world!");
+    let func_type = if cfg!(feature = "docker") {
+        FunctionType::Docker
+    } else {
+        FunctionType::Wasm
+    };
+
+    run_function(exponential, func_type);
+}
+
+fn exponential(x: f64) -> String {
+    let exp: f64 = f64::exp(x);
+    format!("{:?}", exp)
 }
