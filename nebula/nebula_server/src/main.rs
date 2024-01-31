d75f3aaa45f0930c9a3569a298c5a5916ae39de8
Marius Kluften <marius@kluften.dev>
2024-01-31 14:00:26 +0100

    revert to normal http

--- nebula/nebula_server/src/main.rs
+++ nebula/nebula_server/src/main.rs
@@ -13,10 +13,7 @@ use axum::{
 use nebula_server::{
     components::function_results::{call_function, get_function_results, AppState},
     pages::{docker_page, index, metrics, wasm_page},
-    utilities::{
-        persist::load_results,
-        redirect_http_to_https::{redirect_http_to_https, Ports},
-    },
+    utilities::persist::load_results,
 };
 use tower_http::services::ServeDir;
 use tracing::info;
@@ -34,15 +31,16 @@ async fn main() -> anyhow::Result<()> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let ports = Ports {
-        http: options.port,
-        https: 443,
-    };
-
-    if cfg!(not(debug_assertions)) && options.host.to_string() == "0.0.0.0" {
-        info!("We in this production mode, running on 0.0.0.0");
-        tokio::spawn(redirect_http_to_https(ports));
-    }
+    // Leave out https redirect for now
+    //
+    // let ports = Ports {
+    //     http: options.port,
+    //     https: 443,
+    // };
+    // if cfg!(not(debug_assertions)) && options.host.to_string() == "0.0.0.0" {
+    //     info!("We in this production mode, running on 0.0.0.0");
+    //     tokio::spawn(redirect_http_to_https(ports));
+    // }
 
     info!("initializing router...");
 
@@ -74,17 +72,10 @@ async fn main() -> anyhow::Result<()> {
         options.host, options.port
     );
 
-    axum::Server::bind(&SocketAddr::new(
-        options.host,
-        if cfg!(debug_assertions) {
-            ports.http
-        } else {
-            ports.https
-        },
-    ))
-    .serve(router.into_make_service())
-    .await
-    .context("error while starting server")?;
+    axum::Server::bind(&SocketAddr::new(options.host, options.port))
+        .serve(router.into_make_service())
+        .await
+        .context("error while starting server")?;
 
     Ok(())
 }
