28f366fddf64926b641ac99483e802fd5a651161
Marius Kluften <marius@kluften.dev>
2024-02-02 16:19:49 +0100

    start on splitting avg times into docker and wasm

--- nebula/nebula_server/src/components/function_results.rs
+++ nebula/nebula_server/src/components/function_results.rs
@@ -24,9 +24,12 @@ pub struct AppState {
 #[template(path = "components/function_results.rs.html")]
 struct FCList {
     function_results: Vec<FunctionResult>,
-    total_invocations: usize,
-    avg_startup: u128,
-    avg_total_time: u128,
+    total_wasm_invocations: usize,
+    total_docker_invocations: usize,
+    avg_wasm_startup: u128,
+    avg_wasm_total_time: u128,
+    avg_docker_startup: u128,
+    avg_docker_total_time: u128,
 }
 
 #[derive(Deserialize)]
@@ -60,21 +63,46 @@ pub async fn call_function(
 
     let function_results: Vec<FunctionResult> = lock.clone().into_iter().rev().collect();
     let _ = save_results(function_results.clone());
-    let total_invocations = function_results.len();
-    let startup_time_sum: u128 = function_results
+    let wasm_results: Vec<FunctionResult> = function_results
+        .clone()
+        .into_iter()
+        .filter(|result| matches!(result.func_type, ModuleType::Wasm))
+        .collect();
+
+    let total_wasm_invocations = wasm_results.len();
+    let wasm_startup_times: u128 = wasm_results
         .iter()
         .map(|result| result.metrics.as_ref().unwrap().startup_time)
         .sum();
-    let runtime_sum: u128 = function_results
+    let wasm_runtime_sum: u128 = wasm_results
+        .iter()
+        .map(|result| result.metrics.as_ref().unwrap().total_runtime)
+        .sum();
+
+    let docker_results: Vec<FunctionResult> = function_results
+        .clone()
+        .into_iter()
+        .filter(|result| matches!(result.func_type, ModuleType::Docker))
+        .collect();
+
+    let total_docker_invocations = docker_results.len();
+    let docker_startup_times: u128 = docker_results
+        .iter()
+        .map(|result| result.metrics.as_ref().unwrap().startup_time)
+        .sum();
+    let docker_runtime_sum: u128 = docker_results
         .iter()
         .map(|result| result.metrics.as_ref().unwrap().total_runtime)
         .sum();
 
     let template = FCList {
         function_results,
-        total_invocations,
-        avg_startup: startup_time_sum / total_invocations as u128,
-        avg_total_time: runtime_sum / total_invocations as u128,
+        total_wasm_invocations,
+        total_docker_invocations,
+        avg_wasm_startup: wasm_startup_times / total_wasm_invocations as u128,
+        avg_docker_startup: docker_startup_times / total_wasm_invocations as u128,
+        avg_wasm_total_time: wasm_runtime_sum / total_wasm_invocations as u128,
+        avg_docker_total_time: docker_runtime_sum / total_wasm_invocations as u128,
     };
 
     HtmlTemplate(template)
