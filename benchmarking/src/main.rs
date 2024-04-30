ef294784e69dbe3a75adc2596bf48a6f700d8f98
Marius Kluften <marius@kluften.dev>
2024-04-30 21:01:12 +0200

    feat: clean up and allow multiple paths based on args

--- benchmarking/src/main.rs
+++ benchmarking/src/main.rs
@@ -1,22 +1,45 @@
-use benchmarking::utils::{file::get_data_path, modbus::read_modbus_data, request::bombard_nebula};
-use reqwest::Client;
-#[allow(unused_imports)]
-use std::process;
+use benchmarking::{
+    baseline::read_baseline,
+    benchmarking::{benchmark_nebula_nrec, benchmark_nebula_rpi},
+    utils::{
+        efficiency_statistics::{analyze_efficiency_data, print_analyzed_efficiency_metrics},
+        energy_statistics::{analyze_power_data, print_analyzed_power_metrics},
+        file::read_values,
+        request::FunctionResult,
+    },
+};
 
 #[tokio::main]
 async fn main() {
-    let client = Client::new();
+    let env_arg = std::env::args().nth(1).unwrap_or("".to_owned());
+    if env_arg == "baseline" {
+        read_baseline().await;
+    } else if env_arg == "nrec" {
+        benchmark_nebula_nrec().await;
+    } else if env_arg == "rpi" {
+        benchmark_nebula_rpi().await;
+    } else if env_arg == "print_rpi" {
+        let previous_readings: Vec<FunctionResult> = read_values("energy_data_rpi").await.unwrap();
 
-    let bombard_handle = tokio::spawn(async move {
-        bombard_nebula(client).await;
-    });
+        let analyzed = analyze_power_data(&previous_readings);
 
-    let read_energy_handle = tokio::spawn(async move {
-        loop {
-            let sensor_data = read_modbus_data("192.168.68.66:502").await.unwrap();
-            println!("Read modbus data: {:?}", sensor_data);
-        }
-    });
-    let _ = tokio::join!(bombard_handle, read_energy_handle);
-    println!("After async");
+        print_analyzed_power_metrics(&analyzed);
+    } else if env_arg == "print_nrec" {
+        let previous_readings: Vec<FunctionResult> = read_values("energy_data_nrec").await.unwrap();
+
+        println!("Found {} readings from the file.", previous_readings.len());
+
+        let analyzed = analyze_efficiency_data(&previous_readings);
+
+        println!(
+            "Ended up with {} records with energy readings from the results.",
+            analyzed.len()
+        );
+
+        print_analyzed_efficiency_metrics(&analyzed);
+    } else {
+        eprintln!(
+            "Please provide arg for the desired benchmarking! Options: baseline | nrec | rpi"
+        );
+    }
 }
