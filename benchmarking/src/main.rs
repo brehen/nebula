bdfeab60868ad14726bbec107ec3f07c14fca8d6
Marius Kluften <marius@kluften.dev>
2024-05-03 11:47:13 +0200

    analyze data instead

--- benchmarking/src/main.rs
+++ benchmarking/src/main.rs
@@ -2,12 +2,22 @@ use benchmarking::{
     baseline::read_baseline,
     benchmarking::{benchmark_nebula_nrec, benchmark_nebula_rpi},
     utils::{
-        efficiency_statistics::{analyze_efficiency_data, print_analyzed_efficiency_metrics},
+        efficiency_statistics::{analyze_efficiency_data, EfficiencyMetrics},
         energy_statistics::{analyze_power_data, print_analyzed_power_metrics},
-        file::read_values,
+        file::{read_values, write_results},
         request::FunctionResult,
     },
 };
+use serde_derive::Serialize;
+
+
+#[derive(Serialize)]
+struct Respi {
+    func_name: String,
+    func_type: String,
+    input: String,
+    metrics: EfficiencyMetrics,
+}
 
 #[tokio::main]
 async fn main() {
@@ -24,19 +34,24 @@ async fn main() {
         let analyzed = analyze_power_data(&previous_readings);
 
         print_analyzed_power_metrics(&analyzed);
-    } else if env_arg == "print_nrec" {
+    } else if env_arg == "analyze_nrec" {
         let previous_readings: Vec<FunctionResult> = read_values("energy_data_nrec").await.unwrap();
 
         println!("Found {} readings from the file.", previous_readings.len());
 
         let analyzed = analyze_efficiency_data(&previous_readings);
 
-        println!(
-            "Ended up with {} records with energy readings from the results.",
-            analyzed.len()
-        );
+        let analyzed_nrec: Vec<_> = analyzed
+            .into_iter()
+            .map(|(k, data)| Respi {
+                func_type: k.0,
+                func_name: k.1,
+                input: k.2,
+                metrics: data,
+            })
+            .collect();
 
-        print_analyzed_efficiency_metrics(&analyzed);
+        let _ = write_results(&analyzed_nrec, "analyzed_nrec_data_0_4_0").await;
     } else {
         eprintln!(
             "Please provide arg for the desired benchmarking! Options: baseline | nrec | rpi"
